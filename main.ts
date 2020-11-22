5f82f4368997ad358f311f46e00c14a2a49b302b
markb139 <mark.burton@pinpoint-software.com>
2020-11-22 19:30:00 +0000

    initial

--- main.ts
+++ main.ts
@@ -1,3 +1,57 @@
-basic.forever(function () {
-	
-})
+
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace dcf77 {
+    const mult: number[] = [1, 2, 4, 8, 10, 20, 40, 80]
+
+    function decodeNumber (bits: number[], offset: number, count: number) {
+        let ret = 0
+        for (let i = 0; i <= count; i++) {
+            ret = ret + mult[i] * bits[i + offset]
+        }
+        return ret
+    }
+
+    export function decodeMinute (bits: number[]) {
+        return decodeNumber(bits, 21, 6)
+    }
+
+    export function decodeHour (bits: number[]) {
+       return decodeNumber(bits, 29, 5)
+    }
+
+    export function decodeDay (bits: number[]) {
+        return decodeNumber(bits, 36, 5)
+    }
+
+    export function decodeMonth (bits: number[]) {
+        return decodeNumber(bits, 45, 4)
+    }
+
+    export function decodeYear (bits: number[]) {
+        return decodeNumber(bits, 50, 7)
+    }
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function convert(n: number): string {
+        if(n < 10) {
+            return "0" + convertToText(n)
+        }
+        else {
+            return convertToText(n)
+        }
+    }
+}
